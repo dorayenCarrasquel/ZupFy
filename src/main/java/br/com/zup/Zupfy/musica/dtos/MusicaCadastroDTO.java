abdf2f2aa7f6940e92c1239c5f08542b78dd7219
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-12-10 14:45:57 -0300

    Add message to Valid tags

--- src/main/java/br/com/zup/Zupfy/musica/dtos/MusicaCadastroDTO.java
+++ src/main/java/br/com/zup/Zupfy/musica/dtos/MusicaCadastroDTO.java
@@ -2,15 +2,16 @@ package br.com.zup.Zupfy.musica.dtos;
 
 import br.com.zup.Zupfy.enuns.Estilo;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class MusicaCadastroDTO {
-    @Size(min = 1)
-    @NotNull
+    @Size(min = 2)
+    @NotBlank(message = "O nome precisa ser preenchido")
     private String nome;
-    @NotNull
+    @NotNull(message = "O tipo precisa ser preenchido")
     private Estilo estilo;
 
     public MusicaCadastroDTO() {
