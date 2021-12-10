66340983c821605c1cbc7ad42136fe1a0fc66dfd
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-12-10 11:37:28 -0300

    Add MensagemDeErro to ControllerAdvise

---
+++ src/main/java/br/com/zup/Zupfy/config/MensagemDeErro.java
@@ -0,0 +1,18 @@
+package br.com.zup.Zupfy.config;
+
+public class MensagemDeErro {
+    private String mensagem;
+
+    public MensagemDeErro(String mensagem) {
+        this.mensagem = mensagem;
+
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
