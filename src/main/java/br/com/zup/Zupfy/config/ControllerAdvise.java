7f579682255e2bbafc28a55097731edec56230ff
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-12-10 14:47:14 -0300

    Add exception handler 422

--- src/main/java/br/com/zup/Zupfy/config/ControllerAdvise.java
+++ src/main/java/br/com/zup/Zupfy/config/ControllerAdvise.java
@@ -2,16 +2,18 @@ package br.com.zup.Zupfy.config;
 
 import br.com.zup.Zupfy.musica.exceptions.MusicaNaoEcontradaExeception;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerAdvise {
 
     @ExceptionHandler(MusicaNaoEcontradaExeception.class)
@@ -30,6 +32,21 @@ public class ControllerAdvise {
         }
 
         return listamensagen;
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MensagemDeErro manipularErroDeSintaxisDoCliente (HttpMessageNotReadableException exception){
+        return new MensagemDeErro("Tem algumos erros de ESCRITA");
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MensagemDeErro manipularExcecaoEnumInvalido(HttpMessageNotReadableException exception){
+        if(exception.getLocalizedMessage().contains("br.com.zup.Zupfy.enuns.Estilo")){
+            return new MensagemDeErro("Opção não encontrada");
+        }
+        return new MensagemDeErro(exception.getLocalizedMessage());
     }
 
 }
